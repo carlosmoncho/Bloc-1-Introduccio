2a1fedeb8fd86ce3da877eead43244cccc80fee9
Carlos Moncho Molla <carlosmoncho2002@gmail.com>
2021-09-16 17:09:00 +0200

    [ADD] Exercisi 1.1 FINAL

--- public/calculadora.php
+++ public/calculadora.php
@@ -1,9 +1,9 @@
 <?php
 require_once ("../kernel.php");
-$x = $_GET['x'];
-$y = $_GET['y'];
-$suma = $x+$y;
-$resta= $x-$y;
-$multiplicacio = $x*$y;
-$divisio= $y? $x/$y:0;
+$x=$_GET['x'];
+$y=$_GET['y'];
+$suma=$x+$y;
+$resta=$x-$y;
+$multiplicacion=$x*$y;
+$division=$y?$x/$y:0;
 require($rutavistes."calculadoraview.php");
