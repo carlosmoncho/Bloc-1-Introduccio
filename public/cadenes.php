b7ad94fb87fe1ac260bce9c6967b8307539877fc
Carlos Moncho Molla <carlosmoncho2002@gmail.com>
2021-09-17 09:46:55 +0200

    [ADD] Exercisi 1.2

--- public/cadenes.php
+++ public/cadenes.php
@@ -1,1 +1,36 @@
 <?php
+require_once ("../kernel.php");
+$nom=$_GET['nom'];
+$prefix=$_GET['prefix'];
+$url = 'http://username:password@hostname:9090/path?arg=value';
+if (!$nom){
+    $nom='Carlos Moncho Molla';
+}
+$nomTrim=trim($nom,"/");
+$nomSesnseEspais=trim($nomTrim,' ');
+$longitudNom=strlen(str_replace(' ','',$nomSesnseEspais));
+$nomMayus=strtoupper($nomTrim);
+$nomMinus=strtolower($nomTrim);
+$nomSubstitucio=str_ireplace('o','0',$nomTrim);
+$numeroA=substr_count($nomMinus,'a');
+$posicionPrimeraA=stripos($nomTrim,'a');
+if (!$posicionPrimeraA){
+    $posicionPrimeraA='No se ha encontrado ninguna a';
+}
+$comparacioNomPrefix=començaPer($nomTrim,$prefix);
+
+function començaPer($nom,$prefix){
+    echo $nom;
+    if ($prefix){
+        $longitud=strlen($prefix);
+        $comparacio=substr($nom,0,$longitud);
+        if (strpos($comparacio,$prefix)!==false){
+            return "Comença per el mateix prefix";
+        }else{
+            return "No comença per el mateix prefix";
+        }
+    }else{
+        return 'No existeix la variable prefix';
+    }
+}
+require($rutavistes."cadenes.view.php");
--- views/cadenes.view.php
+++ views/cadenes.view.php
@@ -1,1 +1,37 @@
-<?php
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <style>
+        body{
+            background-color: #6868b3;
+            padding: 2em;
+            text-align: center;
+        }
+
+
+    </style>
+</head>
+<body>
+<header>
+    <h2>
+        <?php
+        print_r('nom= '.$nomTrim.'<br>'.
+            $longitudNom.'<br>'.
+            $nomMayus.'<br>'.
+            $nomMinus.'<br>'.
+            $nomSubstitucio.'<br>'.
+            $numeroA.'<br>'.
+            $posicionPrimeraA.'<br>'.
+            $comparacioNomPrefix.'<br>');
+        print_r(parse_url($url, PHP_URL_QUERY).'<br>');
+        print_r(parse_url($url, PHP_URL_SCHEME).'<br>');
+        print_r(parse_url($url, PHP_URL_USER).'<br>');
+        print_r(parse_url($url, PHP_URL_PATH).'<br>');
+        ?>
+    </h2>
+</header>
+</body>
+</html>
